396ef00f4bce1bf3d1a23194c474cba31373b095
wizzbuzz <139072309+wizzbuzz@users.noreply.github.com>
2025-08-15 23:46:20 +0200

    Almost finished

--- process-txt.py
+++ process-txt.py
@@ -1,11 +1,37 @@
 from collections import Counter
 import json
+from time import sleep
 import dcl
 import pandas as pd
 import spacy
 import genanki
+import requests
+from bs4 import BeautifulSoup
 
 nlp = spacy.load("es_core_news_md")
+
+def extract_definition(response):
+    data = response["query"]["pages"][str(next(iter(response["query"]["pages"])))]["extract"]
+    soup = BeautifulSoup(data, features="html.parser")
+    return(soup.dd.get_text().splitlines()[0])
+
+def get_definition(word):
+    api_url = f"https://es.wiktionary.org/w/api.php?action=query&format=json&prop=extracts&titles={word}"
+    response = requests.get(api_url)
+    # Check if word has an entry
+    success = False
+    try:
+        response.json()["query"]["pages"]["-1"] == None
+    except:
+        success = True
+    # Break if entry is missing
+    if(success == False):
+        raise Exception("No definition found")
+    
+    definition = extract_definition(response.json())
+    # extract_definition(response)
+    
+    return definition
 
 def clean_string(string, chars):
     # Remove characters in chars
@@ -32,14 +58,11 @@ def get_difficulty(maxFrequency, word):
     difficulty_stop = 0 if word["stop"] == False else 25
     difficulty = difficulty_frequency + difficulty_length + difficulty_diacritic + difficulty_stop
     
-    if(debug == True):
-        print(difficulty, difficulty_frequency, difficulty_length, difficulty_diacritic, difficulty_stop)
-    
     return difficulty
 
 debug = True
-show_CSV = False
-file_location = "avatar.txt"
+show_CSV = True
+file_location = "input.txt"
 desired_language = "es"
 desired_difficulty_rating = 30
 
@@ -49,7 +72,7 @@ if(debug == False):
     # Verify txt file location
     try:
         with open(file_location):
-            print("Success!")
+            print("successs!")
     except:
         file_location = input(f"File {file_location} not found, please enter txt file location: ")
     # User inputs language
@@ -63,12 +86,16 @@ if(debug == True):
 
 # Save file to string, remove non-letters
 file_as_string = ""
-with open(file_location) as f:
-    file_as_string = clean_string(string=f.read(), chars=".,0123456789:->¡!¿?")
+with open(file_location, encoding="latin-1") as f:
+    file_as_string = clean_string(string=f.read(), chars=".,0123456789:->¡!¿?½")
 
 # Lemmatize  
-doc = nlp(file_as_string)
+doc = nlp(file_as_string.replace("\n", " "))
 
+sentences_in_script = ""
+with open(file_location, encoding="latin-1") as f:
+    sentences_in_script = f.read().splitlines()
+    
 # Program creates a word list of txt file
 c = Counter()
 max_frequency = float("-inf")
@@ -78,26 +105,38 @@ for token in doc:
     if (token.pos_ not in set(["SPACE", "AUX","PROPN", "PRON", "SCONJ","ADV", "CCONJ", "DET", "ADP"])):
         # Increase frequency
         c[token.lemma_] += 1
+        
+        example_sentence = ""
+        #Try getting example sentence from script
+        try: 
+            example_sentence = next(sentence for sentence in sentences_in_script if token.text in sentence)
+        except:
+            example_sentence = ""
+        
         # Add object to wordlist
-        word_list[token.lemma_] = {
+        word_list[token.lemma_.split(" ")[0]] = {
             # Add lemma
-            "word": token.lemma_,
+            "word": token.lemma_.split(" ")[0],
             # Add frequency
-            "frequency": c[token.lemma_],
+            "frequency": c[token.lemma_.split(" ")[0]],
             # Add length
-            "length": len(token.lemma_),
+            "length": len(token.lemma_.split(" ")[0]),
             # Add diacritic count
-            "diacriticCount": dcl.count_diacritics(token.lemma_),
+            "diacriticCount": dcl.count_diacritics(token.lemma_.split(" ")[0]),
             # Add part of speech
             "POS": token.pos_,
             # Add is stop
             "stop": token.is_stop,
+            # Add sentence in script
+            "sentence in script": example_sentence
         }
         
         # Update max frequency if it is higher than the old value
         if(c[token.lemma_] > max_frequency):
             max_frequency = c[token.lemma_]
+            
 
+# Calculate difficulty for every word
 word_list_difficulty_stripped = {}
 for x, obj in word_list.items():
     difficulty = get_difficulty(max_frequency, obj)
@@ -105,8 +144,23 @@ for x, obj in word_list.items():
     if(obj["difficulty"] >= desired_difficulty_rating):
         word_list_difficulty_stripped[x] = obj
 
+# Get wiktionario 
+word_list_with_definition = {}
+i = 0
+for x, obj in word_list_difficulty_stripped.items():
+    i += 1
+    sleep(1)
 
+    try: 
+        print(obj["word"])
+        obj["definition"] = get_definition(obj["word"])
+        word_list_with_definition[obj["word"]] = obj
+        # print(word_list_with_definition)
+        print(f"{i} out of {len(word_list_difficulty_stripped.items())} done...")
+    except:
+        print("There is no definition!")
+        continue
 
 if (show_CSV == True):
-    df = pd.DataFrame.from_dict(word_list_difficulty_stripped, orient="index")
-    df.to_csv("debug2.csv", encoding="utf-8", errors="replace")
+    df = pd.DataFrame.from_dict(word_list_with_definition, orient="index")
+    df.to_csv("debug6.csv", encoding="latin-1", errors="replace")
