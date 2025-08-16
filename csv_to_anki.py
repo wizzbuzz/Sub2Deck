9fe89613984ff842bf778c2bbb66be6d761cab1c
wizzbuzz <139072309+wizzbuzz@users.noreply.github.com>
2025-08-16 11:15:55 +0200

    Polish 1

--- csv_to_anki.py
+++ csv_to_anki.py
@@ -2,37 +2,36 @@ import random
 import genanki
 import csv
 
-file = open(input("Please enter .csv input file: "))
-anki_file_name = input("Please enter Anki Deck name: ")
-reader = csv.reader(file, delimiter=",")
+def main():
+  file = open(input("Please enter .csv input file: "))
+  anki_file_name = input("Please enter Anki Deck name: ")
+  reader = csv.reader(file, delimiter=",")
 
-model = genanki.Model(
-  1607392319,
-  'Simple Model',
-  fields=[
-    {'name': 'Question'},
-    {'name': 'Answer'},
-  ],
-  templates=[
-    {
-      'name': 'Card 1',
-      'qfmt': '{{Question}}',
-      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
-    },
-  ])
+  model = genanki.Model(
+    1607392319,
+    'Simple Model',
+    fields=[
+      {'name': 'Question'},
+      {'name': 'Answer'},
+    ],
+    templates=[
+      {
+        'name': 'Card 1',
+        'qfmt': '{{Question}}',
+        'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+      },
+    ])
 
-deck = genanki.Deck(
-  random.randrange(1 << 30, 1 << 31),
-  anki_file_name)
+  deck = genanki.Deck(
+    random.randrange(1 << 30, 1 << 31),
+    anki_file_name)
 
-for row in reader:
-    print(row)
-    note = genanki.Note(
-        model=model,
-        fields=[row[0], row[-1]]
-    )
-    print(note)
-    deck.add_note(note)
+  for row in reader:
+      note = genanki.Note(
+          model=model,
+          fields=[row[0], row[-1]]
+      )
+      deck.add_note(note)
 
-print(deck)
-genanki.Package(deck).write_to_file(f'{anki_file_name}.apkg')
+  genanki.Package(deck).write_to_file(f'{anki_file_name}.apkg')
+  print(f"Succesfully created {anki_file_name}.apkg")
